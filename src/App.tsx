4f92ee766ef1a3e9bbdd37edb0f0485ca2a5b3e3
YenNhi <yennhinguyentran.2306@gmail.com>
2026-03-13 16:20:18 +0700

    feat: update project configuration for deployment and routing

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ function App() {
         <div className="min-h-screen flex justify-center bg-background bg-gradient-to-bl dark:from-background/95 to-accent/30 bg-[length:200%_200%] animated-gradient">
             <div className="w-full max-w-4xl px-3 py-8 sm:py-8 lg:px-0 gap-4 flex flex-col sm:gap-8">
                 <ThemeProvider>
-                    <BrowserRouter>
+                    <BrowserRouter basename="/billsplitter">
                         <AppRoutesWrapper />
                     </BrowserRouter>
                 </ThemeProvider>
